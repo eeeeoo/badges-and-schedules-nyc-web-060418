c7e6c1c9ff05c83a57ca08d8c7668aeedb079bfa
Jee Yoon Lee <jlee66@wellesley.edu>
2018-06-04 11:25:57 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array << badge_maker(name)
+  end
+  new_array
+end
+
+def assign_rooms(array)
+  new_array = []
+  array.each_with_index do |name, i|
+    new_array << "Hello, #{name}! You'll be assigned to room #{i+1}!"
+  end
+  new_array
+end
+
+def printer(array)
+  batch_badge_creator(array).zip(assign_rooms(array)).each do |badge, name|
+    puts badge
+    puts name
+  end
+end
